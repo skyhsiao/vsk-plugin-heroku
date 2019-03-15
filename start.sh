5b5276c30fa2a69f69e7d51d58c9a218be488ca2
Dark11296 <35867750+Dark11296@users.noreply.github.com>
2019-03-15 18:52:48 +0800

    Add files via upload

--- start.sh
+++ start.sh
@@ -20,8 +20,8 @@ if [[ -z "${Method}" ]]; then
   Method="aes-256-gcm"
 fi
 
-if [[ -z "${SSPW}" ]]; then
-  SSPW="herokushadow"
+if [[ -z "${SS_PW}" ]]; then
+  SS_PW="herokushadow"
 fi
 
 date -R
@@ -46,19 +46,13 @@ sed -i -E "s/^user .*/user $(whoami);/" /etc/nginx/nginx.conf
 
 SYS_Bit="$(getconf LONG_BIT)"
 
-if [ "$VER" = "latest" ]; then
-  V_VER=`wget -qO- "https://api.github.com/repos/v2ray/v2ray-core/releases/latest" | grep 'tag_name' | cut -d\" -f4`
-else
-  V_VER="v$VER"
-fi
+V_VER=`wget -qO- "https://api.github.com/repos/v2ray/v2ray-core/releases/latest" | grep 'tag_name' | cut -d\" -f4`
 
 wget --no-check-certificate -qO '/tmp/v2ray.zip' "https://github.com/v2ray/v2ray-core/releases/download/$V_VER/v2ray-linux-$SYS_Bit.zip"
 wget --no-check-certificate -qO '/tmp/demo.tar.gz' "https://github.com/Dark11296/vsk-plugin-heroku/raw/master/demo.tar.gz"
-wget --no-check-certificate -qO '/tmp/v2ray-plugin.tar.gz' "https://github.com/Dark11296/vsk-plugin-heroku/raw/master/v2ray-plugin-linux-$SYS_Bit.tar.gz"
 
 unzip /tmp/v2ray.zip -d /tmp
 tar xvf /tmp/demo.tar.gz -C /wwwroot
-tar xvf /tmp/v2ray-plugin.tar.gz -C /home/Software
 
 if [ ! -d /tmp/v2ray-$V_VER-linux-$SYS_Bit ]; then
   CorePath="/tmp"
@@ -72,51 +66,6 @@ cp "${CorePath}/geoip.dat" "/home/Software/geoip.dat"
 cp "${CorePath}/geosite.dat" "/home/Software/geosite.dat"
 chmod +x /home/Software/*
 
-function version_lt() { test "$(echo "$@" | tr " " "\n" | sort -r | head -n 1)" != "$1"; }
-
-if version_lt $V_VER "v4.0"; then
-cat <<-EOF > /home/Software/config.json
-{
-    "log":{
-        "loglevel":"warning"
-    },
-    "inbound":{
-        "protocol":"vmess",
-        "port":2333,
-        "settings":{
-            "clients":[
-                {
-                    "id":"${UUID}",
-                    "level":1,
-                    "alterId":${AlterID}
-                }
-            ]
-        },
-        "streamSettings":{
-            "network":"ws",
-            "wsSettings":{
-                "path":"${V2_Path}"
-            }
-        }
-    },
-    "outbound":{
-        "protocol":"freedom",
-        "settings":{}
-    },
-    "policy":{
-        "levels":{
-            "1":{
-                "handshake":10,
-                "connIdle":300,
-                "uplinkOnly":0,
-                "downlinkOnly":0,
-                "bufferSize":0
-            }
-        }
-    }
-}
-EOF
-else
 cat <<-EOF > /home/Software/config.json
 {
     "log":{
@@ -141,15 +90,66 @@ cat <<-EOF > /home/Software/config.json
                     "path":"${V2_Path}"
                 }
             }
+        },
+        {
+            "protocol":"shadowsocks",
+            "port":9000,
+            "settings":{
+                "ota": false,
+                "network":"tcp,udp",
+                "method":"${Method}",
+                "password":"${SS_PW}",
+                "level":1
+            },
+            "streamSettings":{
+                "network":"domainsocket"
+            }
+        },
+        {
+            "protocol":"dokodemo-door",
+            "port":8080,
+            "settings":{
+                "address":"v1.mux.cool",
+                "followRedirect":false,
+                "network":"tcp"
+            },
+            "tag":"ws_in",
+            "streamSettings":{
+                "network":"ws",
+                "security":"none",
+                "wsSettings":{
+                    "path":"${SS_Path}"
+                }
+            }
         }
     ],
     "outbounds":[
         {
             "protocol":"freedom",
-            "settings":{
+            "settings":{}
+        },
+        {
+            "protocol":"freedom",
+            "tag":"ws_out",
+            "streamSettings":{
+                "network":"domainsocket"
             }
         }
     ],
+    "transport":{
+        "dsSettings":{
+            "path":"/home/Software/ss-loop.sock"
+        }
+    },
+    "routing":{
+        "rules":[
+            {
+                "type": "field",
+                "inboundTag":["ws_in"],
+                "outboundTag":"ws_out"
+            }
+        ]
+    }, 
     "policy":{
         "levels":{
             "1":{
@@ -163,21 +163,6 @@ cat <<-EOF > /home/Software/config.json
     }
 }
 EOF
-fi
 
-cat <<-EOF > /home/Software/ss.json
-{
-    "server":"0.0.0.0",
-    "server_port":8080,
-    "local_port":1080,
-    "password":"${SSPW}",
-    "timeout":120,
-    "method":"${Method}",
-    "plugin":"/home/Software/v2ray-plugin_linux_amd64",
-    "plugin_opts":"server;mode=websocket;path=${SS_Path};loglevel=none"
-}
-EOF
-
-ss-server -c /home/Software/ss.json &
 /home/Software/v2ray &
 supervisord --nodaemon --configuration /etc/supervisord.conf
